0e40c2c687f0441e4df896ae82ef5c6cbe1c91bd
Mehdy Laughzey <92257878+MehdyLaughzey@users.noreply.github.com>
2022-05-28 02:02:18 -0500

    Just changing

--- graphicTrainings/main.py
+++ graphicTrainings/main.py
@@ -1,4 +1,20 @@
-from cgi import test
-
-
-this is a test
+# Program to plot a Circle
+# using Parametric equation of a Circle
+ 
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+theta = np.linspace( 0 , 2 * np.pi , 150 )
+ 
+radius = 0.4
+ 
+a = radius * np.cos( theta )
+b = radius * np.sin( theta )
+ 
+figure, axes = plt.subplots( 1 )
+ 
+axes.plot( a, b )
+axes.set_aspect( 1 )
+ 
+plt.title( 'Parametric Equation Circle' )
+plt.show()
